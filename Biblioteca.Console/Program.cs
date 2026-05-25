82038a1dc5594d0386e75ee9ad6e7fdb365b243a
“samu009” <samuele.iudica009@gmail.com>
2026-05-25 10:58:01 +0200

    implementation of the program class

---
+++ Biblioteca.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Biblioteca.Domain;
+using Brani.Domain;
+
+namespace Biblioteca.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                string inputTitolo = Console.ReadLine();
+                string inputAutore = Console.ReadLine();
+                int inputDurata = int.Parse(Console.ReadLine());
+                string t = Console.WriteLine("premi 1 se vuoi aggiungere un altro brano al cd o 0 per finire:");
+                if(t== "1")
+                {
+                    Brano brano = new Brano(inputTitolo, inputAutore, inputDurata);
+                    CD cd = new CD("CD1", "Autore1");
+                    cd.listaBrani.Add(brano);
+                }
+            } while (s == true);
+        }
+    }
+}
